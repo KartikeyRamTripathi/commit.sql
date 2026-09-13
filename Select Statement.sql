d45a244e26b2e07c9f229342a374c3eaeb1b054c
Kartikey Ram Tripathi <tripathikartikey883@gmail.com>
2026-09-13 12:21:19 +0530

    Add files via upload

---
+++ Select Statement.sql
@@ -0,0 +1,33 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name
+FROM employee_demographics;
+
+SELECT first_name, last_name
+FROM employee_demographics;
+
+SELECT last_name, 
+first_name, 
+gender, 
+age
+FROM employee_demographics;
+
+SELECT first_name, 
+last_name,
+salary,
+salary + 100
+FROM employee_salary;
+
+SELECT first_name, 
+last_name,
+salary,
+(salary + 100) * 10
+FROM employee_salary;
+
+
+SELECT department_id
+FROM employee_salary;
+
+SELECT DISTINCT department_id
+FROM employee_salary;
